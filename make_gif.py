8e43cea2b7b5a58349a4f099b3e2d697cc4f8c47
Pedro M. Lohigorry <pedrolohigorry@gmail.com>
2022-07-23 14:30:42 -0300

    change duration to 400

--- make_gif.py
+++ make_gif.py
@@ -4,7 +4,7 @@ def make_gif(frame_folder):
     frames = [Image.open(image) for image in glob.glob(f"/Users/Usuario/Desktop/images/*.png")]
     frame_one = frames[0]
     frame_one.save("my_awesome2.gif", format="GIF", append_images=frames,
-               save_all=True, duration=300, loop=0)
+               save_all=True, duration=400, loop=0)
 
 if __name__ == "__main__":
     make_gif("/path/to/images")
