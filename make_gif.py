4fda7295f9a4dbeecd5153cf31d3616c69765903
Pedro M. Lohigorry <pedrolohigorry@gmail.com>
2022-08-11 00:30:10 -0300

    Change path to desktop so that it works in Linux and Windows

--- make_gif.py
+++ make_gif.py
@@ -20,13 +20,18 @@ set to 1.
 '''
 
 import glob
+import os
 from PIL import Image
+
 def make_gif(interval = 400, looping = 0):
-    frames = [Image.open(image) for image in glob.glob(f"/Users/Usuario/Desktop/images/*.png")]
+    path_to_desktop = os.path.join(os.environ["HOMEPATH"], "Desktop", "images", "*.png")
+    frames = [Image.open(image) for image in glob.glob(path_to_desktop)]
     frame_one = frames[0]
 
     frame_one.save("animation.gif", format="GIF", append_images=frames,
                save_all=True, duration=interval, loop=looping)
 
+
 if __name__ == "__main__":
-    make_gif("/Users/Usuario/Desktop/images/*.png")
+    path_to_desktop = os.path.join(os.environ["HOMEPATH"], "Desktop", "images", "*.png")
+    make_gif(path_to_desktop)
