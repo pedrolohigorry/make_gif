7a121cb8c8c239a78da96d2cc162deef82fb27fd
Pedro M. Lohigorry <pedrolohigorry@gmail.com>
2022-08-11 00:11:54 -0300

    Add two optional parameters: interval and looping

--- make_gif.py
+++ make_gif.py
@@ -1,10 +1,32 @@
+'''
+####################################
+Instructions:
+-  In your laptop create a folder named 'images' and put inside there all there
+.png files that you want to convert into a .gif file.
+
+-  The script will follow
+the order of the files in the folder. I recommend that the files are ordered by
+time or that you add in front of them '01', '02', '03', etc.
+
+-  There are two optional variables:
+1) interval: this is the time interval between images. The default value is
+400 ms.
+2) looping: this value makes the gif animation to continue forever if it's set
+to 0 (default value). Or it makes the animation to stop after one cycle if it's
+set to 1.
+
+- The exit file will be named 'animation.gif'.
+#####################################
+'''
+
 import glob
 from PIL import Image
-def make_gif(frame_folder):
+def make_gif(interval = 400, looping = 0):
     frames = [Image.open(image) for image in glob.glob(f"/Users/Usuario/Desktop/images/*.png")]
     frame_one = frames[0]
-    frame_one.save("my_awesome2.gif", format="GIF", append_images=frames,
-               save_all=True, duration=400, loop=0)
+
+    frame_one.save("animation.gif", format="GIF", append_images=frames,
+               save_all=True, duration=interval, loop=looping)
 
 if __name__ == "__main__":
-    make_gif("/path/to/images")
+    make_gif("/Users/Usuario/Desktop/images/*.png")
